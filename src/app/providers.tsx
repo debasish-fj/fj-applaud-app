400e25ef55a608cf44b651bd676fc18ad8fba67c
Debasish Mishra <debasish.mishra@fncr.com>
2026-04-21 15:33:13 +0530

    (feat) add app providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "./context/theme";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
+    <SessionProvider refetchOnWindowFocus={false}>
+      <ThemeProvider>
+        <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
